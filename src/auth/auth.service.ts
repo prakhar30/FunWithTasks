5e7513ad1a31813d04cf7f487e3dc73b755a68cb
Prakhar Tripathi <prakhar.tripathi@cred.club>
2022-08-24 01:09:41 +0530

    added basic signup api

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import { User } from './user.entity';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    @InjectRepository(User) private tasksRepository: Repository<User>,
+  ) {}
+
+  async signUp(authcredentialsDto: AuthCredentialsDto): Promise<void> {
+    const { username, password } = authcredentialsDto;
+    const user = this.tasksRepository.create({ username, password });
+    await this.tasksRepository.save(user);
+  }
+}
